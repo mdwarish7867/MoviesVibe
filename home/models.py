dad737ecaf98cadc15e04303014de4171bb214a6
mdwarish7867 <warishansari018@proton.me>
2025-04-27 13:55:34 +0530

    solve migrate error

--- home/models.py
+++ home/models.py
@@ -1,8 +1,4 @@
 from django.db import models
-
-# Create your models here.
-
-from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator  # Add this import
